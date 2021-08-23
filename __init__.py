9cf069c41d778364a6462c85b6b90fdd59ab094a
mrSmithG <gbengamosun@gmail.com>
2021-08-23 12:19:58 +0100

    Changes and updates to current app

---
+++ __init__.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+from flask_login import LoginManager
+
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'main.login'
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    # attach routes and custom error pages here
+    from .app import main as main_blueprint
+    app.register_blueprint(main_blueprint, url_prefix='/main')
+
+    return app
